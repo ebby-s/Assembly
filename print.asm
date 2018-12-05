3e2fbfa5429a678019264b98dfd4bd91eca6ff31
ebby-s <31300178+ebby-s@users.noreply.github.com>
2018-12-05 20:38:10 +0800

    Update print.asm

--- print.asm
+++ print.asm
@@ -1,24 +1,53 @@
 ldm 4
 xch r14
-ldm 1
+ldm 7
 xch r15
 ldm 3
 xch r12
-clb
+ldm 10
+xch r13
 
 main:
 jms $3f0
-ld r2
+xch r2
 jcn az finish
-
-
-jcn c1 add_one
+xch r1
+jms process_char
+xch r0
+xch r2
+xch r1
+xch r3
+xch r1
+jms $3e0
+jms process_char
+xch r0
+xch r2
+xch r1
+xch r3
+jms $3e0
 jun main
 
-add_one:
-inc r0
-clc
-jun main
+
+process_char:
+ld r1
+sub r13
+jcn c1 letters
+jcn c0 numbers
+
+letters:
+clb
+ld r14
+xch r0
+ld r1
+add r15
+xch r1
+bbl
+
+numbers:
+clb
+ld r12
+xch r0
+bbl
 
 finish:
 jms $3ff
