9d567faf6b4dc4e8b3204c8993028dc8e39925f8
ebby-s <31300178+ebby-s@users.noreply.github.com>
2018-12-08 07:50:13 +0800

    Update randomiser.asm

--- randomiser.asm
+++ randomiser.asm
@@ -1,46 +1,37 @@
 ldm 1
 xch r3
-ldm 2
-xch r4
-ldm 15
-xch r15
+ldm 5
+xch r5
 jun dec_count
 
 main:
 clb
-xch r1
+ld r1
 add r3
+xch r3
 xch r1
-xch r0
+ld r0
 add r2
 ral
 clc
 rar
-xch r0
-xch r1
-xch r3
-xch r1
-xch r0
 xch r2
 xch r0
 jun dec_count
 
 dec_count:
-clb
 ld r5
 jcn az dec_higher
-xch r5
 dac
 xch r5
 jun main
 
 dec_higher:
-clb
-xch r4
+ld r4
 jcn az finish
 dac
 xch r4
-ld r15
+ldm 15
 xch r5
 jun main
 
